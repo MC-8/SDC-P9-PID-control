589e3579b9b50e05e2c2001e310adb9cf39db582
Max Curzi <massimiliano.curzi@gmail.com>
2018-08-04 18:39:45 +0100

    Basic working PID controller to keep car on track with 0.6 throttle

--- src/PID.cpp
+++ src/PID.cpp
@@ -5,17 +5,30 @@ using namespace std;
 /*
 * TODO: Complete the PID class.
 */
-
+#define TIMESTEP_S (0.001) // myTODO time?
 PID::PID() {}
 
 PID::~PID() {}
-
-void PID::Init(double Kp, double Ki, double Kd) {
+static double prev_cte = 0;
+void PID::Init(double Kp, double Ki, double Kd)
+{
+    PID::Kp = Kp;
+    PID::Ki = Ki;
+    PID::Kd = Kd;
+    p_error = 0;
+    i_error = 0;
+    d_error = 0;
 }
 
-void PID::UpdateError(double cte) {
+void PID::UpdateError(double cte)
+{
+    p_error = cte * Kp;
+    i_error += cte * Ki * TIMESTEP_S;
+    d_error = (cte - prev_cte)/TIMESTEP_S * Kd;
+    prev_cte = cte;
 }
 
-double PID::TotalError() {
+double PID::TotalError()
+{
+    return p_error + i_error + d_error;
 }
-
